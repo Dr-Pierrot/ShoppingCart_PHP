4ac6c3bab255dd113ef761dc58e445c1b3f44e07
Dr-Pierrot <Capulongako16@gmail.com>
2022-11-25 07:13:22 +0800

    successfully created the remover confirm and adjusted some files

--- cart.php
+++ cart.php
@@ -1,6 +1,8 @@
 <?php
     session_start();
-    require'products.php';
+    require_once('products.php');
+    
+    // session total amount must be reinitialize to 0 everytime this page loads so that our computations will work
     $_SESSION['totalAmount'] = 0;
 
     if(isset($_POST['btnUpdate'])) {
@@ -29,12 +31,7 @@
             }
         }
     }
-
 ?>
-
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -42,113 +39,112 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="css/shoppingcart.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" integrity="sha512-P5MgMn1jBN01asBgU0z60Qk4QxiXo86+wlFahKrsQf37c9cro517WzVSPPV1tDKzhku2iJ2FVgL67wG03SGnNA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/styles.css">
     <title>ArmYourSelf</title>
 </head>
 <body>
-    <div class="container">
-        <div class="row mt-3">
-            <div class="col-10">
-                <h1><i class="fa-solid fa-gun"></i>ArmYourSelf Online Shop</h1>
+    <form method="post">
+        <div class="container">
+            <div class="row mt-5">
+                <div class="col-10">
+                    <h1><i class="fa fa-store"></i> ArmYouSelf</h1>
+                </div>
+                <div class="col-2 text-right">
+                    <a href="cart.php" class="btn btn-primary">
+                        <i class="fa fa-shopping-cart"></i> Cart
+                        <span class="badge badge-light">
+                            <?php echo (isset($_SESSION['totalQuantity']) ? $_SESSION['totalQuantity'] : "0"); ?>
+                        </span>
+                    </a>
+                </div>            
             </div>
-            <div class="col-2 text-right">
-                <a href="cart.php" class="btn btn-primary">
-                    <i class="fa fa-shopping-cart"></i> Cart
-                    <span class="badge bg-light text-dark">
-                        <?php echo (isset($_SESSION['totalQuantity']) ? $_SESSION['totalQuantity'] : "0"); ?>
-                    </span>
-                </a>
-            </div>            
-        </div>
-        <hr>
-    
-        <div class="row">
-            <div class="col-sm-12">
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th class="col-1"></th>
-                            <th class="col-4">Product</th>
-                            <th class="col-2">Color</th>
-                            <th class="col-2">Quantity</th>
-                            <th class="col-2">Price</th>
-                            <th class="col-2">Total</th>
-                            <th class="col-1"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php 
-                            if (isset($_SESSION['cartItems'])) {
-                                foreach ($_SESSION['cartItems'] as $key => $cartitem) {
-                                    foreach ($cartitem as $color => $quantity) {
-                                        $_SESSION['totalAmount'] += ($arrProducts[$key]['price'] * $quantity);
-                                    ?>
-                                        <tr>
-                                            <td><img src="img/<?php echo $arrProducts[$key]['img1'] ?>" class="img-thumbnail"></td>
-                                            <td><h4 ><?php echo $arrProducts[$key]['item']; ?></h4></td>
-                                            <td><?php echo $color; ?></td>
-                                            <td class="col-1">
-                                                <input type="number" name="upqty" value="<?php echo $quantity; ?>" class="form-control text-center" style="width: 150px;">
-                                            </td>
-                                            <td><?php echo '₱ '.$arrProducts[$key]['price']; ?></td>
-                                            <td><?php echo $arrProducts[$key]['price'] * $quantity ?></td>
-                                            
-                                            <td class="col-sm-1 col-md-1">
-                                            <a href="remove-confirm.php" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
-                                            </td>
-                                        </tr>
+            <hr>
 
-                        <?php       }
-                                }
-                            }else{ ?>
-                                <tr>
-                                    <td colspan="7">
-                                        No item has been add to cart yet!
-                                    </td>
-                                </tr>  
-                    <?php   }
-
-                            ?>
-                        <tr>
-                            <?php if(isset($_SESSION['totalQuantity'])): ?>
-                            <td colspan="2">   </td>
-                            <td><h3>Total</h3></td>
+            <div class="row">
+                <div class="col-12">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col-1"> </th>
+                                <th scope="col-3">Product</th>
+                                <th scope="col-1">Size</th>
+                                <th scope="col-2">Quantity</th>
+                                <th scope="col-2">Price</th>
+                                <th scope="col-2">Total</th>
+                                <th scope="col-1"> </th>
+                            </tr>
+                        </thead>
+                        <tbody>
                             
-                            <td class="text-center"><?php echo $_SESSION['totalQuantity']; ?></td>
-                            <td> ---- </td>
-                            <td><?php echo $_SESSION['totalAmount']; ?></td>
-                        </tr>
-                        <?php endif;?>
+                            <!-- this is the part where we assemble our cart intl_get_error_message
+                            we started it by testing out if the cartItems is set, meaning if the user has already purchased something
+                            then we perfom a nested foreach in order to extract all the values we need from our session cartItems
+                            remember that the structure of our session cartitems goes like this
+                            $_SESSION['cartItems'][key][size] = quantity -->
 
-                    </tbody>
-                </table>
-                    <div class="row">
-                        <div class="col-4">
-                            <a href="index.php" class="btn btn-danger form-control">
-                                <i class="fa fa-shopping-bag"></i>
-                                Continue Shopping
-                            </a> 
-                        </div>
-                        
-                        <div class="col-4">
-                            <button type="button" class="btn btn-success form-control">
-                                <i class="fa fa-edit"></i>
-                                Update cart
-                                <span class="glyphicon glyphicon-play"></span>
-                            </button> 
-                        </div>
+                            <?php if(isset($_SESSION['cartItems'])): ?>
+                                <?php foreach($_SESSION['cartItems'] as $key => $value): ?>
+                                    <?php foreach($value as $size => $quantity): ?>
 
-                        <div class="col-4">
-                            <a href="clear.php" class="btn btn-primary form-control">
-                                <i class="fa fa-sign-out-alt"></i>
-                                Checkout
-                            </a>  
-                        </div>
+                                        <!-- this is where we recompute our sesstion totalamount value based on the price of the selected item multiplied by the number of purchased item -->
+                                        <?php $_SESSION['totalAmount'] += $arrProducts[$key]['price'] * $quantity; ?>                                        
+                                        
+                                        <!-- following are just print outs of values and components that should appear in the cart table -->
+                                        <tr>                                        
+                                            <td><img src="img/<?php echo $arrProducts[$key]['img1']; ?>" class="img-thumbnail" style="height: 50px;"></td>
+                                            <td><?php echo $arrProducts[$key]['item']; ?></td>
+                                            <td><?php echo $size; ?></td>
+                                            <td>
+                                                <!-- we need to create three input elements so that we can perform updates on the number of purchases
+                                                each input elements are responsible to supply the necessary values (key, size and quantity) in order for the update to work -->
+                                                <input type="hidden" name="hdnKey[]" value="<?php echo $key; ?>">
+                                                <input type="hidden" name="hdnSize[]" value="<?php echo $size; ?>">
+                                                <input type="number" name="txtQuantity[]" value="<?php echo $quantity; ?>" class="form-control text-center" min="1" max="100" required style="width: 150px;">
+                                            </td>
+                                            <td>₱ <?php echo number_format($arrProducts[$key]['price'], 2); ?></td>
+                                            <td>₱ <?php echo number_format($arrProducts[$key]['price'] * $quantity, 2); ?></td>
+                                            
+                                            <!-- the remove link will contain also the same three get variable parameters in order for our product delition to work -->
+                                            <td><a href="remove-confirm.php?<?php echo 'k=' . $key . '&s=' . $size . '&q=' . $quantity; ?>" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a></td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                <?php endforeach; ?>
+                                    <tr>
+                                        <td colspan="2"> </td>
+                                        <td><b>Total</b></td>
+                                        <td><?php echo $_SESSION['totalQuantity']; ?></td>
+                                        <td>----</td>
+                                        <td><b>₱ <?php echo number_format($_SESSION['totalAmount'], 2); ?><b></td>
+                                        <td>----</td>
+                                    </tr>
+                            <?php else: ?>
+                                <tr>
+                                    <td colspan="7">Cart is still Empty</td>
+                                </tr>
+                            <?php endif; ?>
+                        </tbody>
+                    </table>                
+                </div>            
+            </div>
+
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-4 col-lg-4">
+                    <a href="index.php" class="btn btn-danger btn-lg btn-block"><i class="fa fa-shopping-bag"></i> Continue Shopping</a>
+                </div>
+                <?php if(isset($_SESSION['cartItems'])): ?>
+                    <div class="col-12 col-sm-12 col-md-4 col-lg-4">
+                        <button type="submit" name="btnUpdate" class="btn btn-success btn-lg btn-block"><i class="fa fa-edit"></i> Update Cart</button>
                     </div>
+                    <div class="col-12 col-sm-12 col-md-4 col-lg-4">
+                        <a href="clear.php" class="btn btn-primary btn-lg btn-block"><i class="fa fa-sign-out-alt"></i> Checkout</a>
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
-    </div>
+    </form>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.bundle.min.js" integrity="sha512-wV7Yj1alIZDqZFCUQJy85VN+qvEIly93fIQAN7iqDFCPEucLCeNFz4r35FCo9s6WrpdDQPi80xbljXB8Bjtvcg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 </html>
